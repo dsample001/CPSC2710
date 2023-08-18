ce1e1f95b78518a1367596a9c21110bec4d11e0c
Daniel Sample <dzs0124@auburn.edu>
2023-08-18 13:40:50 -0500

    Added private instance variables.

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,4 +1,10 @@
 public calss SeatReservation {
+
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+    
 public static void main(String[] args) {
 
 }
