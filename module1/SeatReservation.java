fb2e67fb97ad223a4387a3bdaa2232d80526c5ed
Daniel Sample <dzs0124@auburn.edu>
2023-08-18 14:37:14 -0500

    Added getters and setters.

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,11 +1,58 @@
-public calss SeatReservation {
+public class SeatReservation {
 
     private String flightDesignator;
     private java.time.LocalDate flightDate;
     private String firstName;
     private String lastName;
     
-public static void main(String[] args) {
+public String getFlightDesignator() {
+return "";
+}
 
+public void setFlightDesignator(String fd) {
+}
+
+public java.time.LocalDate getFlightDate() {
+return flightDate;
+}
+
+public void setFlightDate(java.time.LocalDate Date) {
+}
+
+public String getFristName() {
+return "";
+}
+
+public void setFirstName(String fn) {
+}
+
+public String getLastName() {
+return "";
+}
+
+public void setLastName(String ln) {
+}
+
+public String toString() {
+    String output = "SeatReservation{flightDesignator=" + flightDesignator;
+    output += ", flightDate=" + flightDate;
+    output += ", ";
+
+    if (firstName.equals(null)) {
+        output += "null";
+    } 
+    else {
+        output += firstName;
+    }
+
+    if (lastName.equals(null)) {
+        output += "null";
+    } 
+    else {
+        output += lastName;
+    }
+
+    output += "}";
+    return output;
 }
 }
