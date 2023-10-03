215c7f1d7354217a689b36138df078d3011218ef
Daniel Sample <dzs0124@auburn.edu>
2023-10-03 14:37:35 -0500

    created and writing to file.

--- module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucComment.java
+++ module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucComment.java
@@ -2,7 +2,10 @@ package edu.au.cpsc.module7.FanucComment;
 
 import javafx.stage.FileChooser;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FanucComment {
 
@@ -17,6 +20,16 @@ public class FanucComment {
         System.out.println("Printed from sub program!");
         System.out.println("File to open: " + opentFile);
         System.out.println("File to create: " + fileToCreate);
+
+        try {
+            BufferedWriter bfWriter = new BufferedWriter(new FileWriter(fileToCreate));
+            bfWriter.write("Test!");
+            bfWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
