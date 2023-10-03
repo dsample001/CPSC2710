4e8b1d04e58c9042f012369c02b05478b259b74e
Daniel Sample <dzs0124@auburn.edu>
2023-10-03 11:41:54 -0500

    -- Passing paths to file to java program.

--- module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucComment.java
+++ module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucComment.java
@@ -8,6 +8,19 @@ public class FanucComment {
 
 
 
+    public FanucComment() {
+
+    }
+
+    public void createFile(String opentFile, String fileToCreate) {
+
+        System.out.println("Printed from sub program!");
+        System.out.println("File to open: " + opentFile);
+        System.out.println("File to create: " + fileToCreate);
+    }
+
+
+
 
 
     }
--- module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucCommentController.java
+++ module7/src/main/java/edu/au/cpsc/module7/FanucComment/FanucCommentController.java
@@ -3,6 +3,7 @@ package edu.au.cpsc.module7.FanucComment;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
@@ -15,27 +16,63 @@ public class FanucCommentController {
 
 
     @FXML
-    private Button fileOpenButton, fileToCreateButton;
+    private Button cancelButton, createFileButton, fileOpenButton, fileToCreateButton;
+
+    @FXML
+    private TextArea infoTextArea;
 
     @FXML
     private TextField fileToOpenTextField, fileToCreateTextField;
 
 
     FileChooser fileChooser = new FileChooser();
+    //File openFile, fileToSave;
 
     @FXML
     protected void onFileOpenClick() throws IOException {
+
+        fileChooser.setTitle("Select Fanuc .ls file to comment out");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Fanuc Ls File", "*.ls"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
         File fileOpen = fileChooser.showOpenDialog(new Stage());
+
+        if (fileOpen.isFile()) {
+            fileToOpenTextField.textProperty().set(fileOpen.getPath());
+        }
+
+
         fileToOpenTextField.textProperty().set(fileOpen.getPath());
+
     }
 
     @FXML
     protected void onFileToCreateClick() throws IOException {
-    File fileToSave = fileChooser.showSaveDialog(new Stage());
+
+        fileChooser.setTitle("Name The Fanuc .LS File To Output");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Fanuc Ls File", "*.ls"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File fileToSave = fileChooser.showSaveDialog(new Stage());
+
         fileToCreateTextField.textProperty().set(fileToSave.getPath());
-        }
+    }
+
+    @FXML
+    protected void onCreateFileClick() throws IOException {
+    FanucComment fc = new FanucComment();
+    String fileToOpenPath = fileToOpenTextField.textProperty().get();
+    String fileToCreatePath = fileToCreateTextField.textProperty().get();
+    fc.createFile(fileToOpenPath, fileToCreatePath);
+
+        //FanucCommentController.generateCommentFile(openFile, fileToSave);
 
 
+    }
+
+    @FXML
+    protected void onCancelClick() throws IOException {
 
 
+    }
 }
